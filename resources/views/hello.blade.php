15f2a4cabb56fc6cbd4761c77fd2753941264bcc
raiyayasunaga <braiya.suket@icloud.com>
2023-02-14 19:14:50 +0900

    firtst

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+</head>
+
+<body>
+  <h1>hellow
+
+  </h1>
+  <?php
+  $hello = "Hello World";
+  print($hello);
+  ?>
+</body>
+
+</html>
+PHPファイル
